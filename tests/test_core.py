467df5f14bb939f46399dbe8eb5fab6d785bf3dd
Kooroush Masoumi <kooroushmasoumi@gmail.com>
2026-05-08 23:16:23 +0330

    expand unit coverage pt2

--- tests/test_core.py
+++ tests/test_core.py
@@ -126,3 +126,5 @@ def test_unregister_stops_execution(clock):
 
 def test_get_missing_job_raises(clock):
     with pytest.raises(JobNotFoundError):
+        CronScheduler(clock=clock).get_job("ghost")
+
